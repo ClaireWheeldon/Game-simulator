9a64c432cd038e38462becc3c0b23d6b71c6431a
ClaireWheeldon <claire.wheeldon@outlook.com>
2020-01-20 15:32:46 +0000

    game play functions updated to reduce redundancy and order code consistently

--- Game simulator/games.py
+++ Game simulator/games.py
@@ -22,7 +22,7 @@ def games_menu():
 		if first_time:
 			print("\nWe have {} games available".format(len(game_name_dict)))
 		else:
-			new_game = validate_input_allowed_list("\nWould you like to play again? Enter Y or N :", ["y", "n"])
+			new_game = validate_input_allowed_list("\nWould you like to play another game? Enter Y or N :", ["y", "n"])
 			if new_game == "N":
 				print("Goodbye, thanks for playing")
 			else:
@@ -89,7 +89,7 @@ def play_game(game_number, game_name_dict, game_desc_dict, game_predict_desc_dic
 	bet_type = "int"
 	bet = validate_input_range(bet_message, bet_minimum, bet_maximum)
 	# Play the game by calling the appropriate game function e.g. game_1 for Flip Coin
-	game_variables = game_call_dict[game_integer](bet, predict, money)
+	game_variables = game_call_dict[game_integer](bet, predict, game_integer, game_name_dict, money)
 	money, message = game_variables
 	print(message)
 	return money
@@ -101,26 +101,27 @@ how much the player is betting on the coin flip.
 This function should also have a parameter that lets the player call either "Heads" or "Tails".
 If the player wins the game, the function should return the amount that they won. 
 If the player loses the game, the function should return the amount that they lost as a negative number."""
-def game_1(bet, predict, money):
-	message = "\nYou played flip a coin.\n"
+def game_1(bet, predict, game_integer, game_name_dict, money):
+	# 1. welcome message
+	message = "\nYou played {}.\n".format(game_name_dict[game_integer])
+	# 2. game play message
 	num = random.randint(1, 2)
-	if num == 1:
-		message += "The coin landed on heads.\n"
-		if predict == "heads":
-			message += "You won £{}.\n".format(str(bet * 2))
-			money = money + (bet * 2)
-		else:
-			message += "You lost £{}.\n".format(str(bet))
-			money = money + (bet * -1)
+	coin = "heads" if num == 1 else "tails"
+	message += "The coin landed on {}.\n".format(coin)
+	# 3. outcome message
+	outcome = coin
+	# 4. result message
+	if predict == outcome:
+		multiplier = 2
+		result = "won"
 	else:
-		message += "The coin landed on tails.\n" 
-		if predict == "tails":
-			message += "You won £{}.\n".format(str(bet * 2))
-			money = money + (bet * 2)
-		else:
-			message += "You lost £{}.\n".format(str(bet))
-			money = money + (bet * -1)
+		multiplier = -1
+		result = "lost"
+	message += "You {} £{}.\n".format(result, str(abs(bet * multiplier)))
+	# 5. money message
+	money = money + (bet * multiplier)
 	message += "Your game money is now £{}.".format(str(money))
+	# 6. return variables
 	game_variables = (money, message)
 	return game_variables
 
@@ -129,27 +130,28 @@ The player predicts whether the sum of those dice is odd or even and wins if the
 The function should have a parameter that allows for the player to guess whether the sum of the two dice is "Odd" or "Even". 
 The function should also have a parameter that allows the player to bet an amount of money on the game."""
 
-def game_2(bet, predict, money):
-	message = "\nYou played cho han.\n"
+def game_2(bet, predict, game_integer, game_name_dict, money):
+	# 1. welcome message
+	message = "\nYou played {}.\n".format(game_name_dict[game_integer])
+	# 2. game play message
 	dice1 = random.randint(1,6)
 	dice2 = random.randint(1,6)
-	sum_die = dice1 + dice2
-	message += "Dice 1 rolled a {}\n".format(str(dice1))
-	message += "Dice 2 rolled a {}\n".format(str(dice2))
-	message += "The sum of the two dice is {}\n".format(str(sum_die))
-	if sum_die % 2 == 0 and predict == "even":
-		message += "The sum of the two dice was even.\n"
-		message += "You won £{}.\n".format(str(bet * 2))
-		money = money + (bet * 2)
-	elif sum_die % 2 != 0 and predict == "odd":
-		message += "The sum of the two dice was odd.\n"
-		message += "You won £{}.\n".format(str(bet * 2))
-		money = money + (bet * 2)
+	message += "Dice 1 rolled a {}\nDice 2 rolled a {}\n".format(str(dice1), str(dice2), str(dice1 + dice2))
+	# 3. outcome message
+	outcome = "even" if (dice1 + dice2) % 2 == 0 else "odd"
+	message += "The sum of the two dice was {}.\n".format(outcome)
+	# 4. result message
+	if outcome == predict:
+		multiplier = 2
+		result = "won"
 	else:
-		message += "The sum of the two dice was not {}.\n".format(predict)
-		message += "You lost £{}.\n".format(str(bet))
-		money = money + (bet * -1)
+		multiplier = -1
+		result = "lost"
+	message += "You {} £{}.\n".format(result, str(abs(bet * multiplier)))
+	# 5. money message
+	money = money + (bet * multiplier)
 	message += "Your game money is now £{}.".format(str(money))
+	# 6. return variables
 	game_variables = (money, message)
 	return game_variables
 
@@ -160,8 +162,10 @@ In this game, there can be a tie.
 What should be returned if there is a tie?
 Check the hint to see an additional challenge for this game. """
 
-def game_3(bet, predict, money):
-	message = "\nYou played pick a card.\n"
+def game_3(bet, predict, game_integer, game_name_dict, money):
+	# 1. welcome message
+	message = "\nYou played {}.\n".format(game_name_dict[game_integer])
+	# 2. game play message
 	suit_dict = {1: "Hearts", 2: "Diamonds", 3: "Spades", 4: "Clubs"}
 	card_dict = {2: "a Two", 3: "a Three", 4: "a Four", 5: "a Five", 6: "a Six", 7: "a Seven", 8: "an Eight", 9: "a Nine", 10: "a Ten", 11: "a Jack", 12: "a Queen", 13: "a King", 14: "an Ace"}
 	pack1_card_value = random.randint(2,12)
@@ -174,37 +178,29 @@ def game_3(bet, predict, money):
 	pack2_suit_name = suit_dict[pack2_suit_value]
 	message += "Your card is {} of {}.\n".format(str(pack1_card_name), str(pack1_suit_name))
 	message += "My card is {} of {}.\n".format(str(pack2_card_name), str(pack2_suit_name))
-	if predict == "higher":
-		if pack1_card_value > pack2_card_value:
-			message += "Your card is the highest. \n"
-			outcome = "won"
-			bet_multiplier = 2
-		elif pack1_card_value == pack2_card_value:
-			message += "Your card is the same value as mine.\n"
-			outcome = "neither"
-			bet_multiplier = 1
-		else:
-			message += "Your card is not the highest.\n"	
-			outcome = "lost"
-			bet_multiplier = -1
-	elif predict == "lower":
-		if pack1_card_value < pack2_card_value:
-			message += "Your card is the lowest. \n"
-			outcome = "won"
-			bet_multiplier = 2
-		elif pack1_card_value == pack2_card_value:
-			message += "Your card is the same value as mine.\n"
-			outcome = "neither won nor lost"
-			bet_multiplier = 1
-		else:
-			message += "Your card is not the lowest.\n"	
-			outcome = "lost"
-			bet_multiplier = -1
-
-	message += "You {} £{}\n".format(outcome, str(abs(bet * bet_multiplier)))
+	# 3. outcome message
+	if pack1_card_value > pack2_card_value:
+		outcome = "higher"
+	elif pack1_card_value == pack2_card_value:
+		outcome = "neither higher or lower"
+	else:
+		outcome = "lower"
+	message += "Your card was {} than mine.\n".format(outcome)
+	# 4. result message
+	if predict == outcome:
+		bet_multiplier = 2
+		result = "won"
+	elif outcome == "neither higher or lower":
+		bet_multiplier = 1
+		result = "drew"
+	else:
+		bet_multiplier = -1
+		result = "lost"
+	message += "You {} £{}.\n".format(result, str(abs(bet * bet_multiplier)))
+	# 5. money message
 	money = money + (bet * bet_multiplier)		
-	
 	message += "Your game money is now £{}.".format(str(money))
+	# 6. return variables
 	game_variables = (money, message)
 	return game_variables
 
@@ -217,36 +213,11 @@ Implement as many rules of roulette as you’d like.
 Make sure to consider the different ways roulette rewards a win. 
 Check the hint to see more about this!"""
 
-def game_4(bet, predict, money):
-	message = "\nYou played roulette.\n"
+def game_4(bet, predict, game_integer, game_name_dict, money):
+	# 1. welcome message
+	message = "\nYou played {}.\n".format(game_name_dict[game_integer])
+	# 2. game play message
 	spin = random.randint(1,38)
-
-# convert predict of 0 or 00 to 37 or 38
-	if predict == "0":
-		predict = "37"
-	elif predict == "00":
-		predict = "38"
-	
-	# calculate whether the spin is even or odd.  
-	# 37 represents 0 and 38 represents 00
-	if spin == 37 or spin == 38:
-		spin_type = "neither odd nor even"
-	elif (spin % 2) == 0: 
-		spin_type = "even"
-	else:
-		spin_type = "odd"
-
-	# calculate whether the spin is red or black.  
-	# 37 represents 0 and 38 represents 00
-	red_numbers = [1, 36, 3, 34, 5, 32, 7, 30, 9, 14, 23, 16, 21, 18, 19, 12, 25, 27]
-	if spin == 37 or spin == 38:
-		spin_colour = "green"
-	elif spin in red_numbers:
-		spin_colour = "red"
-	else:
-		spin_colour = "black"
-
-	#display what number the wheel landed on
 	if spin == 37:
 		spin_name = "0"
 	elif spin == 38:
@@ -254,35 +225,60 @@ def game_4(bet, predict, money):
 	else:
 		spin_name = spin
 	message += "The number on the wheel was {}.\n".format(str(spin_name))
-
-	#for predict = Odd or Even, calculate if there is a win
+	# 3. outcome message
+	# calculate whether the spin is even or odd, 37 represents 0 and 38 represents 00
+	if spin == 37 or spin == 38:
+		spin_type = "neither odd nor even"
+	elif (spin % 2) == 0: 
+		spin_type = "even"
+	else:
+		spin_type = "odd"
+	# calculate whether the spin is red or black, 37 represents 0 and 38 represents 00
+	red_numbers = [1, 36, 3, 34, 5, 32, 7, 30, 9, 14, 23, 16, 21, 18, 19, 12, 25, 27]
+	if spin == 37 or spin == 38:
+		spin_colour = "green"
+	elif spin in red_numbers:
+		spin_colour = "red"
+	else:
+		spin_colour = "black"
+	# depending upon predict return relevant outcome message
 	if predict == "odd" or predict == "even":
 		message += "The number on the wheel was {}.\n".format(spin_type)
+	elif predict == "red" or predict == "black":
+		message += "The number on the wheel was {}.\n".format(spin_colour)
+	# 4. result message
+	# if predict is odd or even, calculate if there is a win
+	if predict == "odd" or predict == "even":
 		if (predict == "odd" and spin_type == "odd") or (predict == "even" and spin_type == "even"):
 			outcome = "won"
 			bet_multiplier = 2
 		else:
 			outcome = "lost"
 			bet_multiplier = -1
-	#for predict = Red or Black, calculate if there is a win 
+	# if predict is red or black, calculate if there is a win 
 	elif predict == "red" or predict == "black":
-		message += "The number on the wheel was {}.\n".format(spin_colour)
 		if (predict == "red" and spin_colour == "red") or (predict == "black" and spin_colour == "black"):
 			outcome = "won"
 			bet_multiplier = 2
 		else:
 			outcome = "lost"
 			bet_multiplier = -1
-	#for predict = a single number, calculate if there is a win 
-	else:	
+	# if predict is a single number, calculate if there is a win, convert predict of 0 or 00 to 37 or 38
+	else:
+		if predict == "0":
+			predict = "37"
+		elif predict == "00":
+			predict = "38"
 		if int(predict) == spin:
 			outcome = "won"
 			bet_multiplier = 10
 		else:
 			outcome = "lost"
-			bet_multiplier = -1	
+			bet_multiplier = -1
+	message += "You {} £{}.".format(outcome, str(abs(bet * bet_multiplier)))
+	# 5. money message
 	money = money + (bet * bet_multiplier)
-	message += "You {} £{}.\n".format(outcome, str(abs(bet * bet_multiplier)))
-	message += "Your game money is now £{}.".format(str(money))
+	message += "\nYour game money is now £{}.".format(str(money))
+	# 6. return variables
 	game_variables = (money, message)
 	return game_variables
